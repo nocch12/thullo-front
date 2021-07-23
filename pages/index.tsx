667c15586946cbb9cce8b36aaeeca99839b85767
Kohki Ohno <k.ohno12.gt@gmail.com>
2021-07-23 17:44:27 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main>test</main>
+
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Home;
